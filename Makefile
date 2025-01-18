6d9fa424ba7623463c736a9a13a496ef090b73ed
Siddeshwar <siddeshwar.work@gmail.com>
2025-01-18 20:03:31 +0530

    game loop

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 build:
-		gcc -nostartfiles -nostdlib -no-pie -c -o snake.o snake.s -g
-		gcc -nostartfiles -nostdlib -no-pie -o snake snake.o -lncurses
+		gcc -nostartfiles -no-pie -c -o snake.o snake.s -g
+		gcc -nostartfiles -no-pie -o snake snake.o -lncurses
 
 clean:
 		rm -f snake snake.o
--- snake.s
+++ snake.s
@@ -5,31 +5,86 @@
 #
 
 .section .data
-msg:      .string "Hello, World!\n"
+.equ SCREEN_WIDTH, 25
+.equ SCREEN_HEIGHT, 20
+.equ KEY_ESC, 27
+
+head:     .string "O"
+pos_x:    .int 0
+pos_y:    .int 0
+dir_x:    .int 1
+dir_y:    .int 0
+
+.section .bss
+.align 8
+win:      .skip 8
 
 .section .text
 .global _start
-.extern initscr keypad noecho nodalay curs_set printw getch endwin
+
+user_input:
+  pushq %rbp
+  movq %rsp, %rbp
+  subq $16, %rsp
+
+  movq win(%rip), %rax
+  movq %rax, %rdi
+  call wgetch
+  movl %eax, -4(%rbp)
+
+  cmpl $KEY_ESC, -4(%rbp)
+  je exit
+
+  leave
+  ret
+
+render:
+  pushq %rbp
+  movq %rsp, %rbp
+
+  call clear
+
+  movq win(%rip), %rdi
+  movl pos_y(%rip), %esi
+  movl pos_x(%rip), %edx
+  movq $head, %rcx
+  call mvprintw
+
+  movq win(%rip), %rdi
+  call refresh
+
+  leave
+  ret
 
 _start:
   call initscr
+  movl %eax, win
 
-  mov %rax, %rdi
+  mov win, %rdi
   mov $1, %rsi
   call keypad
 
   call noecho
 
-  call nodelay
-
-  mov $0, %rdi
+  mov $0, %edi
   call curs_set
 
-  mov $msg, %rdi
-  call printw
+  mov win, %rdi
+  mov $1, %rsi
+  call nodelay
 
-  call getch
 
+game_loop:
+  call user_input
+
+  call render
+
+  mov $1000000, %edi
+  call usleep
+
+  jmp game_loop
+
+exit:
   call endwin
 
   mov $60, %rax
